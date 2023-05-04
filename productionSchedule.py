7b7084cb23053beb6156b2348e42e720edc84d0c
annasol88 <aniamonika@yahoo.com>
2023-05-04 22:33:51 +0100

    updating batch entry form

--- productionSchedule.py
+++ productionSchedule.py
@@ -7,5 +7,7 @@ import json
 
 def create_batch(form):
     print(form)
-    # validate prod line doesnt have anything scheduled simultaneously
+    # validate prod line doesnt have another batch scheduled simultaneously
+    # prod_end = start_time + prod_duration
+    # batch_no = IRVyymm9999
     return 'batch IRVyymm9999 scheduled'
