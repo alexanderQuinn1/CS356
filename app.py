26be095467bb5ac71c27123513f58f3c6c695b54
annasol88 <aniamonika@yahoo.com>
2023-06-01 19:06:11 +0100

    cs356g-54 : wip

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import math
 
 from flask import Flask, render_template, request
 import database_connection as db
+import prod_line_monitor_processor as plm
 import models.batch as batch
 import models.maintenance_operation as maintenance
 import models.passage_qa as qa
@@ -16,54 +17,10 @@ def run_app():
 
 @app.route('/prod-line-monitor', methods=['GET', 'POST'])
 def run_production_line():
-    # get production line
+    heading = 'Production Line Monitor'
+    # get production line from URL params
     pl = 'A'
-    # get batch
-    b = {
-        'batch_id': 'IRV99999999',
-        'prod_type': 'x',
-        'current_stage': 'passage',
-    }
-    if request.method == 'GET':
-        # do this for expansion, todo: this should use ID's for expansion 1 or 2
-        if b['current_stage'] == 'expansion':
-            # get operating params
-            p = {
-                'min_temp': 36.5,
-                'max_temp': 37.5
-            }
-            # get flask monitors
-            m = {
-                'flaskA': {
-                    'id'
-                    'temp': 37,
-                    'ph': 7,
-                    'osmolality': 369,
-                },
-                'flaskB': {
-                    'id'
-                    'temp': 37
-                },
-                'flaskC': {
-                    'id'
-                    'temp': 37
-                },
-                'flaskD': {
-                    'id'
-                    'temp': 37
-                }
-            }
-            return render_template('expansion-monitor.html', heading='Production Line Monitor', prod_line=pl, batch=b, monitor=m, product=p)
-        # do this for passage, todo: this should use ID's for passage 1 or 2 or end of line
-        elif b['current_stage'] == 'passage':
-            # get monitor
-            m = {
-                'peristaltic_pump': 'low',
-                'cell_count': 122777282828229828292
-            }
-            # get qa
-            qa = None
-            return render_template('passage-monitor.html', heading='Production Line Monitor', prod_line=pl, batch=b, monitor=m, qa_data=qa)
+    return plm.render_prod_activity(pl, heading)
 
 
 @app.route('/prod-schedule')
@@ -83,11 +40,12 @@ def run_maintenance_log():
 
 @app.route('/qa-entry', methods=['GET', 'POST'])
 def run_quality_assurance_entry():
+    heading = 'Enter QA Test Data'
     if request.method == 'GET':
-        return render_template('qa-entry.html')
+        return render_template('qa-entry.html', heading=heading)
     else:
         response = qa.save_qa(request.form)
-        return render_template('qa-entry.html', response=response)
+        return render_template('qa-entry.html', heading=heading, response=response)
 
 
 @app.route('/maintenance-entry', methods=['GET', 'POST'])
@@ -101,11 +59,18 @@ def run_maintenance_entry():
 
 @app.route('/batch-schedule-entry', methods=['GET', 'POST'])
 def run_batch_schedule_entry():
+    heading = 'schedule a batch'
     if request.method == 'GET':
-        return render_template('batch-schedule-entry.html')
+        return render_template('batch-schedule-entry.html', heading=heading)
     else:
         response = batch.schedule_batch(request.form)
-        return render_template('batch-schedule-entry.html', response=response)
+        return render_template('batch-schedule-entry.html', heading=heading, response=response)
+
+
+@app.route('/update_passage_monitor', methods=['GET', 'POST'])
+def run_passage_monitor_entry():
+    if request.method == 'GET':
+        return render_template('passage-monitor-entry.html', heading='Enter Passage Monitoring Data')
 
 
 if __name__ == '__main__':
