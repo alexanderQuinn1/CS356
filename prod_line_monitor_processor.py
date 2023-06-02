67ac428474a5998979f9c1b3ea3c39d213f2b382
aayza <aayzaahmed.wfh@gmail.com>
2023-06-02 17:54:30 +0100

    CS356G-58 and 9

--- prod_line_monitor_processor.py
+++ prod_line_monitor_processor.py
@@ -23,37 +23,25 @@ def render_batch_manufacture(heading, prod_line):
         'prod_type': 'x',
         'current_stage': 3,
     }
-    if b['current_stage'] == 2 or b['current_stage'] == 4 or b['current_stage'] == 6:
-        return render_expansion_monitor(heading, prod_line, b)
-    elif b['current_stage'] == 3 or b['current_stage'] == 5 or b['current_stage'] == 7:
-        return render_passage_monitor(heading, prod_line, b)
-    else:
-        #TODO show something if the stage is start
-        return None
-
-
-def render_expansion_monitor(heading, prod_line, batch):
-        # TODO get operating params for the batch
+    # do this for expansion, todo: this should use ID's for expansion 1 or 2
+    if b['current_stage'] == 'expansion':
+        # get operating params
         p = {
-            'min_temp': 36,
-            'max_temp': 38,
-            'min_ph': 6,
-            'max_ph': 8,
-            'min_osmolality': 360,
-            'max_osmolality': 420
+            'min_temp': 36.5,
+            'max_temp': 37.5
         }
-        # TODO get flask monitoring data for the current expansion
+        # get flask monitors
         m = {
             'flasks': [
                 {
                     'id': 12345678,
                     'temp': 37,
-                    'ph': 8.2,
+                    'ph': 7,
                     'osmolality': 369,
                 },
                 {
                     'id': 12345679,
-                    'temp': 35,
+                    'temp': 37,
                     'ph': 7,
                     'osmolality': 369,
                 },
@@ -70,7 +58,7 @@ def render_expansion_monitor(heading, prod_line, batch):
                     'osmolality': 369,
                 }]
         }
-        return render_template('expansion-monitor.html', heading=heading, prod_line=prod_line, batch=batch, monitor=m,
+        return render_template('expansion-monitor.html', heading=heading, prod_line=prod_line, batch=b, monitor=m,
                                product=p)
     # do this for passage, todo: this should use ID's for passage 1 or 2 or end of line
     elif b['current_stage'] == 'passage':
@@ -80,23 +68,9 @@ def render_expansion_monitor(heading, prod_line, batch):
             'cell_count': '1.63x10^9'
         }
         # get qa
-        qa = {
-            'status': 'pass',
-            'colour': 'green'
-        }
+        qa = None
+        # qa = {
+        #     'status': 'pass',
+        #     'colour': 'green'
+        # }
         return render_template('passage-monitor.html', heading=heading, prod_line=prod_line, batch=b, monitor=m, qa=qa)
-
-
-def render_passage_monitor(heading, prod_line, batch):
-    # TODO get passage monitoring data for the current passage
-    m = {
-        'peristaltic_pump': 'low',
-        'cell_count': '1.63x10^9'
-    }
-    # TODO get qa for the current passage if any
-    qa = None
-    # qa = {
-    #     'status': 'pass',
-    #     'colour': 'green'
-    # }
-    return render_template('passage-monitor.html', heading=heading, prod_line=prod_line, batch=batch, monitor=m, qa=qa)
