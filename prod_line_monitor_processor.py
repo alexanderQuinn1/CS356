365aef18609c6ed042bebbef0e3dad591947ae4d
aayza <aayzaahmed.wfh@gmail.com>
2023-06-02 17:47:52 +0100

    CS356G-58 and 9

--- prod_line_monitor_processor.py
+++ prod_line_monitor_processor.py
@@ -1,16 +1,39 @@
 from flask import render_template
 
 
-def render_prod_activity(prod_line, heading):
-    # get batch
-    b = {
-        'batch_id': 'IRV99999999',
-        'prod_type': 'x',
-        'current_stage': 'expansion',
+def render_prod_activity(heading, prod_line):
+    #TODO fetch prod activity from DB
+    prod_activity = {
+        'type': 'batch_manufacture'
     }
-    # do this for expansion, todo: this should use ID's for expansion 1 or 2
-    if b['current_stage'] == 'expansion':
-        # get operating params
+    if prod_activity['type'] == 'batch_manufacture':
+        return render_batch_manufacture(heading, prod_line)
+    elif prod_activity['type'] == 'maintenance':
+        #TODO
+        return None
+    else:
+        #TODO
+        return None
+
+
+def render_batch_manufacture(heading, prod_line):
+    # TODO get batch currently in production on prod_line from DB
+    b = {
+        'batch_no': 'IRV99999999',
+        'prod_type': 'x',
+        'current_stage': 3,
+    }
+    if b['current_stage'] == 2 or b['current_stage'] == 4 or b['current_stage'] == 6:
+        return render_expansion_monitor(heading, prod_line, b)
+    elif b['current_stage'] == 3 or b['current_stage'] == 5 or b['current_stage'] == 7:
+        return render_passage_monitor(heading, prod_line, b)
+    else:
+        #TODO show something if the stage is start
+        return None
+
+
+def render_expansion_monitor(heading, prod_line, batch):
+        # TODO get operating params for the batch
         p = {
             'min_temp': 36.5,
             'max_temp': 37.5
@@ -45,17 +68,18 @@ def render_prod_activity(prod_line, heading):
         }
         return render_template('expansion-monitor.html', heading=heading, prod_line=prod_line, batch=b, monitor=m,
                                product=p)
-    # do this for passage, todo: this should use ID's for passage 1 or 2 or end of line
-    elif b['current_stage'] == 'passage':
-        # get monitor
-        m = {
-            'peristaltic_pump': 'low',
-            'cell_count': '1.63x10^9'
-        }
-        # get qa
-        qa = None
-        # qa = {
-        #     'status': 'pass',
-        #     'colour': 'green'
-        # }
-        return render_template('passage-monitor.html', heading=heading, prod_line=prod_line, batch=b, monitor=m, qa=qa)
+
+
+def render_passage_monitor(heading, prod_line, batch):
+    # TODO get passage monitoring data for the current passage
+    m = {
+        'peristaltic_pump': 'low',
+        'cell_count': '1.63x10^9'
+    }
+    # TODO get qa for the current passage if any
+    qa = None
+    # qa = {
+    #     'status': 'pass',
+    #     'colour': 'green'
+    # }
+    return render_template('passage-monitor.html', heading=heading, prod_line=prod_line, batch=batch, monitor=m, qa=qa)
